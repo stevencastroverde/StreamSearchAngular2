bc7d7be761b7d81f9a0ea5ef1e1bad1accce6c7e
Steven Castroverde <stevencastroverde@Stevens-MacBook-Air.local>
2017-04-22 23:09:27 -0600

    added route for landing page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,39 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { FooterComponent } from './footer/footer.component';
+import { MediaSearchComponent } from './media-search/media-search.component';
+import { SingleMovieComponent } from './single-movie/single-movie.component';
+import { SingleShowComponent } from './single-show/single-show.component';
+import { SearchResultsComponent } from './search-results/search-results.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+
+//Router
+const appRoutes: Routes = [
+  { path:'' , component: LandingPageComponent },
+  { path: '', component: }
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    FooterComponent,
+    MediaSearchComponent,
+    SingleMovieComponent,
+    SingleShowComponent,
+    SearchResultsComponent,
+    LandingPageComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
