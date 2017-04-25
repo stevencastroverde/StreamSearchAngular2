12b134876a53100497307e6422caae9ce17a494c
Steven <sfcastroverde@iMac.local>
2017-04-24 18:18:05 -0600

    added http service

--- src/app/services/guide-box.service.ts
+++ src/app/services/guide-box.service.ts
@@ -1,24 +1,37 @@
 import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
-import { Observable } from  'rxjs/Observable';
+import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
 
 @Injectable()
 export class GuideBoxService {
-  private _baseURL: string = 'https://serene-gorge-20513.herokuapp.com';
+  private _baseURL = 'https://serene-gorge-20513.herokuapp.com';
 
   constructor(private http: Http) { }
-  getFreeMovies(): Observable<any>{
+  getFreeMovies(): Observable<any> {
     return this.http.get(`${this._baseURL}/movies/free}`)
       .map(res => res.json());
   };
-  getFreeShows(): Observable<any>{
+  getFreeShows(): Observable<any> {
     return this.http.get(`${this._baseURL}/shows/free`)
       .map(res => res.json());
   };
-  searchMovies(searchTerm: string): Observable<any>{
+  searchMovies(searchTerm: string): Observable<any> {
     return this.http.get(`${this._baseURL}/movies/search/${searchTerm}`)
       .map(res => res.json());
   };
+  getSpecificMovie(movieId: number): Observable<any> {
+    return this.http.get(`${this._baseURL}/movies/${movieId}`)
+      .map(res => res.json());
+  };
+  searchShows(searchTerm: string): Observable<any> {
+    return this.http.get(`${this._baseURL}/shows/search/${searchTerm}`)
+      .map(res => res.json());
+  };
+  getSpecificShow(showId: number, subscriptions: string): Observable<any> {
+    return this.http.get(`${this._baseURL}/shows/${showId}/${subscriptions}`)
+      .map(res => res.json());
+  }
+
 
 }
