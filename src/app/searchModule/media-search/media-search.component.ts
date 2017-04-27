5d49caaa328801c9cbe9e8573e5c6584579eebeb
Steven <sfcastroverde@iMac.PK5001Z>
2017-04-27 13:20:19 -0600

    can search movies and shows

--- src/app/searchModule/media-search/media-search.component.ts
+++ src/app/searchModule/media-search/media-search.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, FormGroup} from '@angular/forms';
+import { Router } from '@angular/router';
+import { GuideBoxService } from '../services/guidebox/guide-box.service';
 
 @Component({
   selector: 'app-media-search',
@@ -6,8 +9,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./media-search.component.scss']
 })
 export class MediaSearchComponent implements OnInit {
+  formModel: FormGroup;
+  currentUrl: string;
 
-  constructor() { }
+  constructor(private route: Router, private GuideBoxService: GuideBoxService) {
+    this.currentUrl = route.url;
+
+    this.formModel = new FormGroup({
+      'search': new FormControl()
+    });
+  }
+
+
+
+
+
+  onSubmit() {
+    if (this.currentUrl === '/shows') {
+      this.GuideBoxService.searchShows(this.formModel.value.search)
+        .subscribe(shows => console.log(shows));
+    } else if (this.currentUrl === '/movies') {
+      this.GuideBoxService.searchMovies(this.formModel.value.search)
+        .subscribe(movies => console.log(movies));
+    } else {
+      this.route.navigateByUrl('/');
+    };
+  };
+
 
   ngOnInit() {
   }
--- src/app/searchModule/movies/movies.component.ts
+++ src/app/searchModule/movies/movies.component.ts
@@ -11,7 +11,7 @@ export class MoviesComponent implements OnInit {
   constructor(private GuideBoxService: GuideBoxService) { }
 
   ngOnInit() {
-    this.getFreeMovies();
+    
   }
 
   getFreeMovies() {
